81385e5759452e3713ab2e21f9f2b7e53a372c26
Oleksii Miroshko <miroshko@live.com>
2014-09-10 01:35:49 +0200

    grid prototype

--- js/app.js
+++ js/app.js
@@ -1,9 +1,10 @@
 (function() {
-  
+  var note_player = new NotePlayer();
   var table_reader = new TableReader(
     document.getElementById('canvas'),
     note_player
   );
-  table
+  table_reader.setTempo(document.getElementById('tempo').value);
+  table_reader.play();
 
 })();
