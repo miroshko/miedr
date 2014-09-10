824217d46a3f804b2d80ee6aef2179c913041e68
Oleksii Miroshko <miroshko@live.com>
2014-09-11 00:25:45 +0200

    added Vue

--- src/js/app.ts
+++ src/js/app.ts
@@ -1,8 +1,21 @@
 /// <reference path="typings/requirejs/require.d.ts" />
+
 require.config({
-  baseUrl: 'js'
+  baseUrl: 'js',
+  paths: {
+    'lodash': '//cdnjs.cloudflare.com/ajax/libs/lodash.js/2.4.1/lodash.min',
+    'vue': '//cdnjs.cloudflare.com/ajax/libs/vue/0.10.6/vue.min'
+  }
 });
 
-require(["classes/Project"], (Project) => {
-  console.log("Piau")
+require(['classes/Project', 'lodash', 'vue'], function(Project, _,  Vue) { 
+  var miedr = new Vue({
+    el: '#the',
+    data: {
+      tempo: 999
+    },
+    created: function() {
+      console.log(100)
+    }
+  });
 })
