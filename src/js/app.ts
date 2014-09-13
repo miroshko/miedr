b915b64b97933f408cd9ac5d7cfefff12254d83b
Oleksii Miroshko <miroshko@live.com>
2014-09-13 14:25:02 +0200

    component for roll

--- src/js/app.ts
+++ src/js/app.ts
@@ -14,12 +14,31 @@ require(['lodash', 'vue', 'Project'], function(_,  Vue, Project) {
   project.name = "Project name";
   project.tempo = 60;
 
+  var pitchRoll = Vue.extend({
+    template: '<ul class="pitch-roll-legend">' +
+        '<li class="key key-{{ $index % 12 }}" data-pitch="{{$index}}" v-repeat="pitches">' +
+        '</li>' +
+      '</ul>' +
+      '<ul class="pitch-roll-grid">' +
+        '<li v-repeat="pitches" class="pitch pitch-{{ $index % 12 }}" v-on="click: $value">' +
+          '{{ $value }}' +
+        '</li>' +
+      '</ul>',
+    created: function() {
+      console.log(this.$data)
+      this.$el.querySelector('.pitch-roll-legend').classList.add('piano-roll')
+      this.$el.querySelector('.pitch-roll-grid').classList.add('piano-grid')
+    }
+  });
+  Vue.component('pitch-roll', pitchRoll);
+
   var miedr = new Vue({
     el: '#the-only-project-so-far',
     data: project,
     methods: {
-      ololo: function(e) {
-        console.log(e);
+      gridClick: function(e) {
+        var posX: Number = e.clientX - e.target.offsetLeft;
+        var posY: Number = e.clientY - e.target.offsetTop;
       }
     }
   });
--- src/js/classes/Track.ts
+++ src/js/classes/Track.ts
@@ -1,7 +1,12 @@
 import Note = require('Note');
 
 export class Track {
-  pitches: Number[] = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24];
+  id: Number;
+  pitches: Note.Note[][] = new Array(24);
   notes: Note.Note[] = [];
   volume: Number = 100;
+  gridClick() {
+    console.log("trololo");
+    return 0;
+  }
 }
