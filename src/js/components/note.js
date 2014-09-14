bd471c07486c3661adf412fcea5aedbfd755d72a
Oleksii Miroshko <miroshko@live.com>
2014-09-15 00:07:03 +0200

    resizing from both sides

--- src/js/components/note.js
+++ src/js/components/note.js
@@ -24,6 +24,25 @@ define(['templates', 'vue', 'interact'], function(templates, Vue, interact) {
           _this.$data.start = new_pos;
         }
       });
+      interact(this.$el.querySelector('.resize-marker-end')).draggable({
+        onmove: function(e) {
+          var new_duration = _this.$data.duration + e.dx / _this.pxRatio;
+          new_duration = Math.max(0, new_duration);
+          _this.$data.duration = new_duration;
+          e.stopPropagation();
+        }
+      });
+      interact(this.$el.querySelector('.resize-marker-begin')).draggable({
+        onmove: function(e) {
+          var new_duration = _this.$data.duration - e.dx / _this.pxRatio;
+          new_duration = Math.max(0, new_duration);
+          var new_pos = _this.$data.start + e.dx / _this.pxRatio;
+          new_pos = Math.max(0, new_pos);
+          _this.$data.duration = new_duration;
+          _this.$data.start = new_pos;
+          e.stopPropagation();
+        }
+      });
     }
   });
   return note;
