55070a3db65487d3410cf6b22c4c32289b89e56d
Oleksii Miroshko <miroshko@live.com>
2014-12-15 03:29:32 +0100

    project moved to its component

--- src/js/app.js
+++ src/js/app.js
@@ -19,9 +19,10 @@ require([
   'vue',
   'classes/Project',
   'components/pianoRoll',
+  'components/project',
   'classes/NoteScheduler',
   'classes/AudioPool'
-  ], function(Vue, Project, pianoRoll, NoteScheduler, AudioPool) {
+  ], function(Vue, Project, pianoRoll, ProjectComponent, NoteScheduler, AudioPool) {
   var audio_pool = new AudioPool();
   var note_scheduler = new NoteScheduler(audio_pool);
   var project = new Project(note_scheduler);
@@ -30,43 +31,10 @@ require([
   project.tempo = 60;
 
   // @todo: move project to project component
-  var miedr = new Vue({
+  var miedr = new ProjectComponent({
     el: '#the-only-project-so-far',
     // is it OK to make the whole project object to the data property?
     data: project,
-    methods: {
-      play: function() {
-        var project = this.$data;
-        var resolution_ms = 100;
-        if(this.playing)
-          return;
-
-        var notes = project.getNotesArray();
-        project.note_scheduler.schedule(project.current_position, project.tempo, notes);
-
-        project.playback_interval = setInterval(function() {
-          project.current_position += 1 * resolution_ms * project.tempo / 60;
-        }, resolution_ms);
-        this.$dispatch('schedule_notes', this.$data);
-        project.playing = true;
-      },
-      pause: function() {
-        clearInterval(this.$data.playback_interval);
-        project.note_scheduler.cancelAllScheduled();
-        this.$data.playing = false;
-        this.playback_interval = null;
-      },
-      stop: function() {
-        this.pause();
-        project.note_scheduler.cancelAllScheduled();
-        this.$data.current_position = 0;
-      }
-    },
-    computed: {
-      px_ratio: function() {
-        return 0.2;
-      }
-    },
     components: {
       // @todo: avoid direct assignment of pitch-roll. Use track
       // 'track': trackComponent
