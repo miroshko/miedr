832d88ee8445e8e73e76b7b076ce8ada7ea75d8a
Oleksii Miroshko <miroshko@live.com>
2015-10-11 15:16:21 +0200

    oscillators destination

--- src/js/classes/NotePlayer.js
+++ src/js/classes/NotePlayer.js
@@ -1,4 +1,20 @@
 define([], function() {
+  return function notePlayerFactory(audioContext) {
+    var oscillators = {};
 
-  return NotePlayer;
+    return {
+      play: function(note) {
+        if (oscillators[note.pitch]){
+          oscillators[note.pitch].stop(0);
+        }
+        oscillators[note.pitch] = audioContext.createOscillator();
+        oscillators[note.pitch].frequency.value = Math.pow(2, (note.pitch - 20 - 49) / 12) * 440;
+        oscillators[note.pitch].connect(audioContext.destination);
+        oscillators[note.pitch].start(0);
+      },
+      stop: function(note) {
+        oscillators[note.pitch].stop(0);
+      }
+    };
+  };
 });
--- tests/spec/NotePlayerSpec.js
+++ tests/spec/NotePlayerSpec.js
@@ -1,3 +1,48 @@
-define([], function() {
+define(['classes/NotePlayer'], function(notePlayerFactory) {
+  describe('Note player', function() {
+    var audioContext = null;
+    var oscillator = null;
+    var NotePlayer = null;
+    var destination = {};
 
+    beforeEach(function() {
+      oscillator = jasmine.createSpyObj('oscillator', ['start', 'stop', 'connect']);
+      oscillator.frequency = {value: null};
+      audioContext = jasmine.createSpyObj('audioContext', ['createOscillator']);
+      audioContext.createOscillator.and.returnValue(oscillator);
+      audioContext.destination = destination;
+      notePlayer = notePlayerFactory(audioContext);
+    });
+
+    it('creates oscillator when play is called', function() {
+      notePlayer.play({pitch: 80});
+      expect(oscillator.start).toHaveBeenCalled();
+    });
+
+    it('connects oscillator to destination', function() {
+      notePlayer.play({pitch: 80});
+      expect(oscillator.connect).toHaveBeenCalledWith(destination);
+    });
+
+    it('creates oscillator with proper frequency', function() {
+      notePlayer.play({pitch: 81});
+      expect(oscillator.frequency.value).toBe(880);
+
+      notePlayer.play({pitch: 69});
+      expect(oscillator.frequency.value).toBe(440);
+    });
+
+    it('stops same note oscillator when play is called', function() {
+      notePlayer.play({pitch: 93});
+      notePlayer.play({pitch: 93});
+      expect(oscillator.stop).toHaveBeenCalled();
+      expect(oscillator.start.calls.count()).toEqual(2);
+    });
+
+    it('stops oscillator when stop is called', function() {
+      notePlayer.play({pitch: 80});
+      notePlayer.stop({pitch: 80});      
+      expect(oscillator.stop).toHaveBeenCalled();
+    });
+  });
 });
